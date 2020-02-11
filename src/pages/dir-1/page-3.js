4e8780f008e42aa038966c5f10a611e779999056
Joseph_Tully <tully4school@gmail.com>
2020-02-11 02:29:06 -0500

    created counter page

---
+++ src/pages/dir-1/page-3.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => 
+<div>
+<h1>This is page 3</h1>
+<Link to="/">Home</Link>
+</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,4 +5,8 @@ export default () =>
 <div>
 <h1>Hello world!</h1>
 <Link to="/page-2/">Page 2</Link>
+<br />
+<Link to="/dir-1/page-3/">Page 3</Link>
+<br />
+<Link to="/dir-1/counter/">Counter</Link>
 </div>
